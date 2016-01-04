a208a38c12b14a45f9668d24d70d66b3780edabb
Dane Everitt <dane@daneeveritt.com>
2016-01-03 22:55:10 -0500

    Allow specifying version as latest

--- start.sh
+++ start.sh
@@ -1,25 +1,29 @@
 #!/bin/ash
 LATEST_VERSION=`curl -s https://s3.amazonaws.com/Minecraft.Download/versions/versions.json | grep -o "[[:digit:]]\.[0-9]*\.[0-9]" | head -n 1`
 
-if [ $VERSION ]; then
-  REQ_VERSION=$VERSION
+if [ -z "$VANILLA_VERSION" ] || [ "$VANILLA_VERSION" -eq "latest" ]; then
+  DL_VERSION=$LATEST_VERSION
 else
-  REQ_VERSION=$LATEST_VERSION
+  DL_VERSION=$VANILLA_VERSION
 fi
 
-FILE=/home/container/server.jar
+if [ -z "$SERVER_JARFILE" ]; then
+    SERVER_JARFILE="server.jar"
+fi
 
-if [ -f $FILE ]; then
-   echo "A server.jar file already exists in this location, not downloading a new one."
+CHK_FILE="/home/container/${SERVER_JARFILE}"
+
+if [ -f $CHK_FILE ]; then
+   echo "A {$SERVER_JARFILE} file already exists in this location, not downloading a new one."
 else
-   curl https://s3.amazonaws.com/Minecraft.Download/versions/$REQ_VERSION/minecraft_server.$REQ_VERSION.jar -o server.jar
+   curl https://s3.amazonaws.com/Minecraft.Download/versions/$DL_VERSION/minecraft_server.$DL_VERSION.jar -o server.jar
 fi
 
 cd /home/container
 
 if [ -z "$STARTUP"  ]; then
     echo "$ java -jar server.jar"
-    java -jar server.jar
+    java -jar ${SERVER_JARFILE}
 else
     MODIFIED_STARTUP=`echo $STARTUP | perl -pe 's@{{(.*?)}}@$ENV{$1}@g'`
     echo "$ java $MODIFIED_STARTUP"
