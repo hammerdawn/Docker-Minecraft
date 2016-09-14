ff6834c8fba6f1f7f6ae3b79242e0376ea7dfbb8
Dane Everitt <dane@daneeveritt.com>
2016-09-14 15:55:15 -0400

    Bug fixes

--- start.sh
+++ start.sh
@@ -8,7 +8,7 @@ cd /home/container
 CHK_FILE="/home/container/${SERVER_JARFILE}"
 
 if [ -f $CHK_FILE ]; then
-   echo "A {$SERVER_JARFILE} file already exists in this location, not downloading a new one."
+   echo "A ${SERVER_JARFILE} file already exists in this location, not downloading a new one."
 else
     if [ -z "$DL_PATH" ] || [ "$DL_PATH" == "build" ]; then
         echo "Building Spigot... This could take awhile."
@@ -16,15 +16,15 @@ else
         cd /tmp
         curl -o BuildTools.jar https://hub.spigotmc.org/jenkins/job/BuildTools/lastSuccessfulBuild/artifact/target/BuildTools.jar
         git config --global --unset core.autocrlf
-        java -jar BuildTools.jar --rev {$DL_VERSION}
+        java -jar BuildTools.jar --rev ${DL_VERSION}
 
-        cp /tmp/spigot-*.jar /home/container/{$SERVER_JARFILE}
+        cp /tmp/spigot-*.jar /home/container/${SERVER_JARFILE}
         cd /home/container
     else
         # Download the file
-        MODIFIED_DL_PATH=`echo $DL_PATH | perl -pe 's@{{(.*?)}}@$ENV{$1}@g'`
-        echo "$ curl --progress-bar -L -o {$SERVER_JARFILE} {$MODIFIED_DL_PATH}"
-        curl --progress-bar -L -o {$SERVER_JARFILE} {$MODIFIED_DL_PATH}
+        MODIFIED_DL_PATH=`echo ${DL_PATH} | perl -pe 's@\{\{(.*?)\}\}@$ENV{$1}@g'`
+        echo "$ curl -L -o ${SERVER_JARFILE} ${MODIFIED_DL_PATH}"
+        curl -L -o ${SERVER_JARFILE} ${MODIFIED_DL_PATH}
     fi
 fi
 
@@ -33,7 +33,7 @@ cd /home/container
 if [ -z "$STARTUP"  ]; then
     echo "error: no startup parameters have been set for this container"
 else
-    MODIFIED_STARTUP=`echo $STARTUP | perl -pe 's@{{(.*?)}}@$ENV{$1}@g'`
-    echo "$ java $MODIFIED_STARTUP"
-    java $MODIFIED_STARTUP
+    MODIFIED_STARTUP=`echo ${STARTUP} | perl -pe 's@\{\{(.*?)\}\}@$ENV{$1}@g'`
+    echo "$ java ${MODIFIED_STARTUP}"
+    java ${MODIFIED_STARTUP}
 fi
