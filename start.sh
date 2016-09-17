54b123f86d43225ba1759fea183908d1d96fae53
Dane Everitt <dane@daneeveritt.com>
2016-09-17 15:28:35 -0400

    Push spongevanilla support

--- start.sh
+++ start.sh
@@ -6,21 +6,11 @@ cd /home/container
 if [ -f $CHK_FILE ]; then
    echo "A ${SERVER_JAR} file already exists in this location, not downloading a new one."
 else
-    if [ ${BUNGEE_VERSION} == "latest" ]; then
-        DL_PATH=http://ci.md-5.net/job/BungeeCord/lastSuccessfulBuild/artifact/bootstrap/target/BungeeCord.jar
-    else
-        DL_PATH=http://ci.md-5.net/job/BungeeCord/${BUNGEE_VERSION}/artifact/bootstrap/target/BungeeCord.jar
-    fi
-
-    echo "> curl -sSL ${DL_PATH} -o ${SERVER_JAR}"
-    curl -sSL ${DL_PATH} -o ${SERVER_JAR}
+    DL_PATH=https://repo.spongepowered.org/maven/org/spongepowered/spongevanilla/${SPONGE_VERSION}/spongevanilla-${SPONGE_VERSION}.jar
+    echo "> curl -sSL ${DL_PATH} -o ${SERVER_JARFILE}"
+    curl -sSL ${DL_PATH} -o ${SERVER_JARFILE}
 fi
 
-if [ -z "$STARTUP"  ]; then
-    echo "$ java -jar ${SERVER_JAR}"
-    java -jar ${SERVER_JAR}
-else
-    MODIFIED_STARTUP=`echo ${STARTUP} | perl -pe 's@\{\{(.*?)\}\}@$ENV{$1}@g'`
-    echo "$ java ${MODIFIED_STARTUP}"
-    java ${MODIFIED_STARTUP}
-fi
+MODIFIED_STARTUP=`echo ${STARTUP} | perl -pe 's@\{\{(.*?)\}\}@$ENV{$1}@g'`
+echo "$ java ${MODIFIED_STARTUP}"
+java ${MODIFIED_STARTUP}
