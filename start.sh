0ae8724870b029050afafe84abb278f8958900cc
Dane Everitt <dane@daneeveritt.com>
2016-09-17 16:14:57 -0400

    fix SERVER_JARFILE naming convention

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
 #!/bin/ash
 sleep 3
-CHK_FILE="/home/container/${SERVER_JAR}"
+CHK_FILE="/home/container/${SERVER_JARFILE}"
 
 cd /home/container
 if [ -f $CHK_FILE ]; then
-   echo "A ${SERVER_JAR} file already exists in this location, not downloading a new one."
+   echo "A ${SERVER_JARFILE} file already exists in this location, not downloading a new one."
 else
     if [ ${BUNGEE_VERSION} == "latest" ]; then
         DL_PATH=http://ci.md-5.net/job/BungeeCord/lastSuccessfulBuild/artifact/bootstrap/target/BungeeCord.jar
@@ -12,13 +12,13 @@ else
         DL_PATH=http://ci.md-5.net/job/BungeeCord/${BUNGEE_VERSION}/artifact/bootstrap/target/BungeeCord.jar
     fi
 
-    echo "> curl -sSL ${DL_PATH} -o ${SERVER_JAR}"
-    curl -sSL ${DL_PATH} -o ${SERVER_JAR}
+    echo "> curl -sSL ${DL_PATH} -o ${SERVER_JARFILE}"
+    curl -sSL ${DL_PATH} -o ${SERVER_JARFILE}
 fi
 
 if [ -z "$STARTUP"  ]; then
-    echo "$ java -jar ${SERVER_JAR}"
-    java -jar ${SERVER_JAR}
+    echo "$ java -jar ${SERVER_JARFILE}"
+    java -jar ${SERVER_JARFILE}
 else
     MODIFIED_STARTUP=`echo ${STARTUP} | perl -pe 's@\{\{(.*?)\}\}@$ENV{$1}@g'`
     echo "$ java ${MODIFIED_STARTUP}"
